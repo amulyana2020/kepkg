5eeae831756f8f7fed57fc12be28ed76c26d0a73
Ai Mulyana <100061184+amulyana2020@users.noreply.github.com>
2024-02-14 13:20:46 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ['kepkg-dev-anha.2.sg-1.fl0.io']
 
