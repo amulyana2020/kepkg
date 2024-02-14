62c78cc159f18de9fb0cd178406438c6f2d1e6ac
Ai Mulyana <100061184+amulyana2020@users.noreply.github.com>
2024-02-14 13:13:18 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env(DEBUG)
 
-ALLOWED_HOSTS = ['https://kepkg-dev-anha.2.sg-1.fl0.io']
+ALLOWED_HOSTS = ['kepkg-dev-anha.2.sg-1.fl0.io']
 
 CSRF_TRUSTED_ORIGINS = ['https://kepkg-dev-anha.2.sg-1.fl0.io']
 # Application definition
